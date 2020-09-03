543a6630ff998f272fe4cd470c63b8aeb41fac24
noah-peeters <noah.ed.peeters@gmail.com>
2020-09-03 21:35:05 +0200

    Extract data from gpx track

---
+++ convert.py
@@ -0,0 +1,23 @@
+import gpxpy
+gpx = gpxpy.parse(open('/home/noah/Music/extra gpx/2020-04-26_14-34_Sun.gpx'))
+
+print("{} track(s)".format(len(gpx.tracks)))
+track = gpx.tracks[0]
+
+print("{} segment(s)".format(len(track.segments)))
+segment = track.segments[0]
+
+print("{} point(s)".format(len(segment.points)))
+
+data = []
+segment_length = segment.length_3d()
+for point_idx, point in enumerate(segment.points):
+    data.append([point.longitude, point.latitude,
+                 point.elevation, point.time, segment.get_speed(point_idx)])
+    
+from pandas import DataFrame
+
+columns = ['Longitude', 'Latitude', 'Altitude', 'Time', 'Speed']
+df = DataFrame(data, columns=columns)
+df.head()
+print(df.to_string())
