1d5dfc573b654bf8dfcb2d79d172dbf29b7f056d
Apenz1 <noah.ed.peeters@gmail.com>
2020-09-09 18:00:16 +0200

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,24 +1,39 @@
+import enum
+from operator import index
 import gpxpy
-gpx = gpxpy.parse(open('/home/noah/Music/extra gpx/2020-04-26_14-34_Sun.gpx'))
+gpx = gpxpy.parse(open('/home/noah/Downloads/Namiddagrit.gpx'))
 
-print("{} track(s)".format(len(gpx.tracks)))
 track = gpx.tracks[0]
-
-print("{} segment(s)".format(len(track.segments)))
 segment = track.segments[0]
 
-print("{} point(s)".format(len(segment.points)))
+#* Get Speeds from gpx file
+speedsList = []
+maxSpeed = 0
+timeSinceStart = 0
+for index, point in enumerate(segment.points):
+    #* Get speed per point (in Km/h)
+    currentSpeed = segment.get_speed(index) * 3.6
+    speedsList.append(currentSpeed)
 
+    #* Get maxSpeed of list
+    if currentSpeed > maxSpeed:
+        maxSpeed = currentSpeed
 
-data = []
-segment_length = segment.length_3d()
-for point_idx, point in enumerate(segment.points):
-    data.append([point.longitude, point.latitude,
-                 point.elevation, point.time, segment.get_speed(point_idx)])
-    
-from pandas import DataFrame
+#* Get Duration of activity
+#prevTime = segment.points[index - 1]
+timeSinceStart = segment.points[len(segment.points) - 1].time_difference(segment.points[0])
+print("Duration of activity:", timeSinceStart / 60, "min")
 
-columns = ['Longitude', 'Latitude', 'Altitude', 'Time', 'Speed']
-df = DataFrame(data, columns=columns)
-df.head()
-print(df.to_string())
+#* Calculate average speed
+speedSum = 0
+for i, v in enumerate(speedsList):
+    speedSum += speedsList[i]
+averageSpeed = speedSum / len(speedsList)   #* Sum of items / Amount of items
+print("AverageSpeed:", averageSpeed, "Km/h")
+print("MaxSpeed:", maxSpeed, "Km/h")
+#from pandas import DataFrame
+
+#columns = ['Longitude', 'Latitude', 'Altitude', 'Time', 'Speed']
+#df = DataFrame(data, columns=columns)
+#df.head()
+#print(df.to_string())
